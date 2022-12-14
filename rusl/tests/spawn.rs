4e849280abf8d57158b772d7bf9d49095dbabed9
gramar <marcus.grass@gmail.com>
2022-12-14 22:01:12 +0100

    Update bad test

--- rusl/tests/spawn.rs
+++ rusl/tests/spawn.rs
@@ -32,14 +32,12 @@ fn test_clone3_vfork() {
 }
 
 #[test]
-fn test_clone3_new_thread() {
+fn test_clone3_pidfd() {
     unsafe {
         let mut pidfd: MaybeUninit<Fd> = MaybeUninit::uninit();
         // Same as above but we're spawning an LVP
-        let mut child_stack = [0u8; 1048];
-        let mut args = Clone3Args::new(CloneFlags::CLONE_VM & CloneFlags::CLONE_PIDFD);
-        args.set_pid_fd(pidfd.as_mut_ptr())
-            .set_stack(&mut child_stack);
+        let mut args = Clone3Args::new(CloneFlags::CLONE_PIDFD);
+        args.set_pid_fd(pidfd.as_mut_ptr());
 
         // In my container, clone3 gives an `ENOSYS` seems to be fairly common container behaviour
         // to keep the sandbox under management
@@ -53,7 +51,7 @@ fn test_clone3_new_thread() {
         if child != 0 {
             let pidfd = pidfd.assume_init();
             // Pidfd is ready to read on complete
-            let done_when = PollFd::new(pidfd, PollEvents::POLLOUT);
+            let done_when = PollFd::new(pidfd, PollEvents::POLLOUT | PollEvents::POLLIN);
             let completed = ppoll(&mut [done_when], None, None).unwrap();
             assert_eq!(1, completed);
         } else {
