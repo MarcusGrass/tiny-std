e28f437b13ea048c8a939b303fc0ab8414961ed1
MarcusGrass <marcus.grass@protonmail.com>
2023-03-03 19:19:35 +0100

    Add devtmpfs mount option

--- rusl/src/platform/compat/mount.rs
+++ rusl/src/platform/compat/mount.rs
@@ -4,6 +4,7 @@ use crate::string::unix_str::{UnixStr};
 pub enum FilesystemType {
     Ext4,
     Tmpfs,
+    Devtmpfs,
     Sysfs,
     Proc,
     Vfat,
@@ -15,6 +16,7 @@ impl FilesystemType {
             match self {
                 FilesystemType::Ext4 => UnixStr::from_str_unchecked("ext4\0"),
                 FilesystemType::Tmpfs => UnixStr::from_str_unchecked("tmpfs\0"),
+                FilesystemType::Devtmpfs => UnixStr::from_str_unchecked("devtmpfs\0"),
                 FilesystemType::Sysfs => UnixStr::from_str_unchecked("sysfs\0"),
                 FilesystemType::Proc => UnixStr::from_str_unchecked("proc\0"),
                 FilesystemType::Vfat => UnixStr::from_str_unchecked("vfat\0"),
