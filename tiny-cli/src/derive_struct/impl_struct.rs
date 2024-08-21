137d7654903edaff5191a314b03ef2cf993b404a
MarcusGrass <marcus.grass@protonmail.com>
2024-08-21 06:35:34 +0200

    tiny-cli Can parse positional arguments, with weird results

--- tiny-cli/src/derive_struct/impl_struct.rs
+++ tiny-cli/src/derive_struct/impl_struct.rs
@@ -1,11 +1,14 @@
 use crate::derive_struct::{
     FieldPackageKind, FieldTy, ParsedField, ParsedSubcommand, StructMetadata,
 };
+use std::collections::VecDeque;
 use std::fmt::Write;
 
 pub(crate) struct CodeWriter {
     printer_head: String,
     printer_mid: String,
+    printer_args: String,
+    printer_arg_help: String,
     printer_opts: Option<String>,
     sc_print_fmt: Option<String>,
     impl_head: String,
@@ -32,6 +35,8 @@ impl CodeWriter {
         Self {
             printer_head: gen_printer_head(&help_printer_name),
             printer_mid,
+            printer_args: String::new(),
+            printer_arg_help: String::new(),
             printer_opts: None,
             sc_print_fmt: None,
             impl_head: gen_impl_head(name, &help_printer_name),
@@ -58,21 +63,32 @@ impl CodeWriter {
     }
 
     fn field_push_printer(&mut self, field: &ParsedField) {
-        if self.printer_opts.is_none() {
-            self.printer_opts = Some("\nOptions:\n".to_string());
+        if let Some(pos) = field.positional.as_ref() {
+            let _ = self
+                .printer_args
+                .write_fmt(format_args!(" [{}]", pos.to_uppercase()));
+            if self.printer_arg_help.is_empty() {
+                self.printer_arg_help.push_str("\nArguments:\n");
+            }
+            field.write_into_args_help(pos, &mut self.printer_arg_help);
+        } else {
+            if self.printer_opts.is_none() {
+                self.printer_opts = Some("\nOptions:\n".to_string());
+            }
+            let Some(opts) = self.printer_opts.as_mut() else {
+                unreachable!()
+            };
+            field.write_into_help(opts);
         }
-        let Some(opts) = self.printer_opts.as_mut() else {
-            unreachable!()
-        };
-        field.write_into_help(opts);
     }
 
     fn field_push_const_match(&mut self, field: &ParsedField) {
-        let _ = self.impl_head.write_fmt(format_args!(
-            "\t\tconst {}: &[u8] = tiny_std::UnixStr::from_str_checked(\"--{}\\0\").as_slice();\n",
-            field.long_const_ident(),
-            field.long_match_lit()
+        if let (Some(lc), Some(lm)) = (field.long_const_ident(), field.long_match_lit()) {
+            let _ = self.impl_head.write_fmt(format_args!(
+            "\t\tconst {lc}: &[u8] = tiny_std::UnixStr::from_str_checked(\"--{lm}\\0\").as_slice();\n",
         ));
+        }
+
         if let (Some(short_const), Some(short_match)) =
             (field.short_const_ident(), field.short_match_lit())
         {
@@ -114,15 +130,16 @@ impl CodeWriter {
     }
 
     fn field_push_to_match(&mut self, field: &ParsedField) {
-        let asgn = member_try_assign(field);
-        let _ = self.match_head.write_fmt(format_args!(
-            "\
-\t\t\t\t{} => {{
+        if let Some(const_match) = field.as_const_match() {
+            let asgn = member_try_assign(field);
+            let _ = self.match_head.write_fmt(format_args!(
+                "\
+\t\t\t\t{const_match} => {{
                     {asgn};
                 }},
 ",
-            field.as_const_match()
-        ));
+            ));
+        }
     }
 
     fn subcommand_push_match_tail(&mut self, sc: &ParsedSubcommand) {
@@ -138,15 +155,22 @@ impl CodeWriter {
 
     fn field_push_to_out(&mut self, field: &ParsedField) {
         if matches!(field.package, FieldPackageKind::None) && !matches!(field.ty, FieldTy::Bool) {
+            let requirement = if let Some(lm) = field.as_lit_match() {
+                format!("\"Required option '{lm}' not supplied.\"")
+            } else if let Some(arg) = field.positional.as_ref() {
+                format!("\"Required argument '{arg}' not supplied.\"")
+            } else {
+                panic!("Neither optional or argument (This is a bug)");
+            };
             let _ = self.struct_out.write_fmt(format_args!("\
             {}: {{
                 if let Some(found_arg) = {} {{
                     found_arg
                 }} else {{
-                    return Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Required option '{}' not supplied.\")?);
+                    return Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), {requirement})?);
                 }}
             }},
-            ", field.name, field.name, field.as_lit_match()));
+            ", field.name, field.name));
         } else {
             let _ = self.struct_out.write_fmt(format_args!(
                 "\
@@ -175,11 +199,12 @@ impl CodeWriter {
         }
     }
 
-    pub(crate) fn finish(mut self) -> String {
+    pub(crate) fn finish(mut self, mut parsed_fields: VecDeque<ParsedField>) -> String {
         let mut output = String::new();
         if self.printer_opts.is_some() {
             let _ = self.printer_mid.write_str(" [OPTIONS]");
         }
+        let _ = self.printer_mid.write_str(&self.printer_args);
         let has_subcommand = self.match_tail.is_some();
         if has_subcommand {
             let _ = self.printer_mid.write_str(" [COMMAND]\n\n{}");
@@ -195,9 +220,23 @@ impl CodeWriter {
                 let _ = output.write_fmt(format_args!("\t\tf.write_fmt(format_args!(\"{}\", <{} as tiny_std::unix::cli::SubcommandParse>::help_printer()))\n", self.printer_mid, sc_ty));
             }
         } else if let Some(opts) = &self.printer_opts {
+            if self.printer_arg_help.is_empty() {
+                let _ = output.write_fmt(format_args!(
+                    "\t\tf.write_str(\"{}{}\")",
+                    self.printer_mid, opts
+                ));
+            } else {
+                // Remove leader newline on opts following args to make it less bloated
+                let use_opt = opts.trim_start_matches('\n');
+                let _ = output.write_fmt(format_args!(
+                    "\t\tf.write_str(\"{}{}{}\")",
+                    self.printer_mid, self.printer_arg_help, use_opt,
+                ));
+            }
+        } else if !self.printer_arg_help.is_empty() {
             let _ = output.write_fmt(format_args!(
                 "\t\tf.write_str(\"{}{}\")",
-                self.printer_mid, opts
+                self.printer_mid, self.printer_arg_help,
             ));
         } else {
             let _ = output.write_fmt(format_args!("\t\tf.write_str(\"{}\")", self.printer_mid));
@@ -215,12 +254,38 @@ impl CodeWriter {
                 }},
 "));
         } else {
-            let _ = self.match_head.write_str("\
-\t\t\t\tb\"-h\\0\" | b\"--help\\0\" => { return Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"\")?) },
-                no_match => {
-                    return Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Unrecognized argument: {:?}\", core::str::from_utf8(no_match)))?);
-                },
-");
+            let mut matched_last = String::new();
+            let mut first = true;
+            loop {
+                let Some(positional) = parsed_fields.pop_front() else {
+                    break;
+                };
+                let as_conv = member_try_assign(&positional);
+                let Some(pos) = positional.positional else {
+                    continue;
+                };
+                let pre = if first { "if" } else { "else if" };
+                let _ = matched_last.write_fmt(format_args!(
+                    "\
+                    {pre} {pos}.is_none() {{
+                        {as_conv};
+                    }}"
+                ));
+                first = false;
+            }
+            let in_match = if matched_last.is_empty() {
+                "return Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Unrecognized argument: {:?}\", core::str::from_utf8(no_match)))?);".to_string()
+            } else {
+                format!("{matched_last} else {{
+                    return Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Unrecognized argument: {{:?}}\", core::str::from_utf8(no_match)))?);
+                }}")
+            };
+            let _ = self.match_head.write_fmt(format_args!("\
+\t\t\t\tb\"-h\\0\" | b\"--help\\0\" => {{ return Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"\")?) }},
+                no_match => {{
+                    {in_match}
+                }},
+"));
         }
         let _ = output.write_str(&self.match_head);
         let _ = output.write_str("\t\t\t}\n\t\t}\n");
@@ -273,54 +338,92 @@ fn member_try_assign(m: &ParsedField) -> String {
 }
 
 fn member_as_convert(m: &ParsedField) -> String {
-    let mut out = String::from("{\n");
-    let _ = out.write_str("\t\t\t\t\t\tlet Some(next_arg) = args.next() else {\n");
-    let _ = out.write_fmt(format_args!(
-        "\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Expected argument following '{}'.\")?);\n",
-        m.as_lit_match()
-    ));
-    let _ = out.write_str("\t\t\t\t\t\t};\n");
-    match &m.ty {
-        FieldTy::UnixStr => {
-            let _ = out.write_str("\t\t\t\t\t\tnext_arg\n");
-            let _ = out.write_str("\t\t\t\t\t\t}");
-        }
-        FieldTy::Str => {
-            let _ = out.write_str("\t\t\t\t\t\tmatch next_arg.as_str() {\n");
-            let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
-            let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
-            let _ = out.write_fmt(format_args!(
-                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument at '{}' as utf8-str\")?);\n",
-                m.as_lit_match()
+    if let Some(lm) = m.as_lit_match() {
+        let mut out = String::from("{\n");
+        let _ = out.write_str("\t\t\t\t\t\tlet Some(next_arg) = args.next() else {\n");
+        let _ = out.write_fmt(format_args!("\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Expected argument following '{lm}'.\")?);\n", ));
+        let _ = out.write_str("\t\t\t\t\t\t};\n");
+        match &m.ty {
+            FieldTy::UnixStr => {
+                let _ = out.write_str("\t\t\t\t\t\tnext_arg\n");
+                let _ = out.write_str("\t\t\t\t\t\t}");
+            }
+            FieldTy::Str => {
+                let _ = out.write_str("\t\t\t\t\t\tmatch next_arg.as_str() {\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument at '{lm}' as utf8-str\")?);\n",
             ));
-            let _ = out.write_str("\t\t\t\t\t\t\t},\n");
+                let _ = out.write_str("\t\t\t\t\t\t\t},\n");
 
-            let _ = out.write_str("\t\t\t\t\t\t}\n");
-            let _ = out.write_str("\t\t\t\t\t}");
+                let _ = out.write_str("\t\t\t\t\t\t}\n");
+                let _ = out.write_str("\t\t\t\t\t}");
+            }
+            FieldTy::Bool => {}
+            FieldTy::Unknown(ty) => {
+                let _ = out.write_str("\t\t\t\t\t\tlet next_str_arg = match next_arg.as_str() {\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument at '{lm}' as utf8-str\")?);\n",
+            ));
+                let _ = out.write_str("\t\t\t\t\t\t\t},\n");
+                let _ = out.write_str("\t\t\t\t\t\t};\n");
+                let _ = out.write_fmt(format_args!(
+                    "\t\t\t\t\t\tmatch <{ty} as core::str::FromStr>::from_str(next_str_arg) {{\n"
+                ));
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Failed to convert argument at '{lm}' from str: {{e}}\"))?);\n",
+            ));
+                let _ = out.write_str("\t\t\t\t\t\t\t}\n");
+                let _ = out.write_str("\t\t\t\t\t\t}\n\t\t\t\t\t}");
+            }
         }
-        FieldTy::Bool => {}
-        FieldTy::Unknown(ty) => {
-            let _ = out.write_str("\t\t\t\t\t\tlet next_str_arg = match next_arg.as_str() {\n");
-            let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
-            let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
-            let _ = out.write_fmt(format_args!(
-                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument at '{}' as utf8-str\")?);\n",
-                m.as_lit_match()
+        out
+    } else {
+        let mut out = String::from("{\n");
+        match &m.ty {
+            FieldTy::UnixStr => {
+                let _ = out.write_str("\t\t\t\t\t\nnext\n");
+                let _ = out.write_str("\t\t\t\t\t\t}");
+            }
+            FieldTy::Str => {
+                let _ = out.write_str("\t\t\t\t\t\tmatch next.as_str() {\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument as utf8-str\")?);\n",
             ));
-            let _ = out.write_str("\t\t\t\t\t\t\t},\n");
-            let _ = out.write_str("\t\t\t\t\t\t};\n");
-            let _ = out.write_fmt(format_args!(
-                "\t\t\t\t\t\tmatch <{ty} as core::str::FromStr>::from_str(next_str_arg) {{\n"
+                let _ = out.write_str("\t\t\t\t\t\t\t},\n");
+
+                let _ = out.write_str("\t\t\t\t\t\t}\n");
+                let _ = out.write_str("\t\t\t\t\t}");
+            }
+            FieldTy::Bool => {}
+            FieldTy::Unknown(ty) => {
+                let _ = out.write_str("\t\t\t\t\t\tlet next_str_arg = match next.as_str() {\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_str(Self::help_printer(), \"Failed to parse argument as utf8-str\")?);\n",
             ));
-            let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
-            let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
-            let _ = out.write_fmt(format_args!(
-                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Failed to convert argument at '{}' from str: {{e}}\"))?);\n",
-                m.as_lit_match()
+                let _ = out.write_str("\t\t\t\t\t\t\t},\n");
+                let _ = out.write_str("\t\t\t\t\t\t};\n");
+                let _ = out.write_fmt(format_args!(
+                    "\t\t\t\t\t\tmatch <{ty} as core::str::FromStr>::from_str(next_str_arg) {{\n"
+                ));
+                let _ = out.write_str("\t\t\t\t\t\t\tOk(s) => s,\n");
+                let _ = out.write_str("\t\t\t\t\t\t\tErr(e) => {\n");
+                let _ = out.write_fmt(format_args!(
+                "\t\t\t\t\t\t\t\treturn Err(tiny_std::unix::cli::ArgParseError::new_cause_fmt(Self::help_printer(), format_args!(\"Failed to convert argument from str: {{e}}\"))?);\n",
             ));
-            let _ = out.write_str("\t\t\t\t\t\t\t}\n");
-            let _ = out.write_str("\t\t\t\t\t\t}\n\t\t\t\t\t}");
+                let _ = out.write_str("\t\t\t\t\t\t\t}\n");
+                let _ = out.write_str("\t\t\t\t\t\t}\n\t\t\t\t\t}");
+            }
         }
+        out
     }
-    out
 }
